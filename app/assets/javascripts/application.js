216e7ed7ccb5c3469c66d1002370fa0225f93a5b
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-08-10 22:03:15 -0500

    Try jquery.turbolinks gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,10 @@
 //
 
 
-
-//= require jquery
-//= require jquery_ujs
 //= require bootstrap
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
